6a645499c60d01575f2cba6c6994e243bbbff329
Ville Kalliokoski <vkalliokoski@gmail.com>
2022-11-11 00:25:25 +0200

    added security

--- pw.sh
+++ pw.sh
@@ -6,5 +6,5 @@ if [ $# < 2 ]
 then
 	echo ${_usage}
 else
-	head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-' | fold -w $1 | head -n 1
+	head -c 500 /dev/urandom | tr -dc 'a-zA-Z0-9~!@#$%^&*_-.;:<>´`£¨`' | fold -w $1 | head -n 1
 fi
